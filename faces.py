d2d10686e28feb99bf717c3e081d6c9c1fe3bee4
Michael <michaelwong121@hotmail.com>
2017-03-09 12:36:27 -0500

    finished majority of part 7 and added code for part 10

--- faces.py
+++ faces.py
@@ -8,6 +8,8 @@ import matplotlib.image as mpimg
 from scipy.ndimage import filters
 import urllib
 from numpy import random
+import time
+import random as rn
 
 from get_data import download_image, split_set
 
@@ -35,6 +37,7 @@ Here is a list of all the parameters that you can change:
 """
 
 np.random.seed(0)
+rn.seed(0)
 
 act =['Fran Drescher', 'America Ferrera', 'Kristin Chenoweth', 'Alec Baldwin', 'Bill Hader', 'Steve Carell']
 
@@ -44,27 +47,15 @@ def getArray (str):
     return(np.array([im.flatten()]))
 
 # this is used to get a mini batch from the training dataset
-def get_train_batch(act, n): # n = image per actor in batch
-    
+def get_train_batch(n, x_train, y_train): # n = image per actor in batch
     x = zeros((0,1024))
     y = zeros((0,6))
-        
-    for k in range(6):
-        name = act[k].split()[1].lower()
-        used_image_no = []
-        while (len(used_image_no) < n):
-            r = random.randint(1, 200)
-            if r in used_image_no:
-                continue
-            filename = name+str(r)+'.'+'jpg'
-            try:
-                x = vstack((x, getArray("part7_training/" + filename)))
-                used_image_no.append(r)
-            except:
-                pass
-        one_hot = zeros(6)
-        one_hot[k] = 1
-        y = vstack((y, tile(one_hot, (n,1))))        
+    
+    idx = rn.sample(range(x_train.shape[0]), n)
+    
+    for k in range(n):
+        x = vstack((x, x_train[idx[k]]))
+        y = vstack((y, y_train[idx[k]]))
     return x, y
     
 
@@ -88,6 +79,7 @@ def get_whole_set(act, file):
 # download, and split dataset
 """
 download_image(act)
+
 split_set(act, 75, 15, 30) # training, val, test
 """
 
@@ -99,7 +91,31 @@ Set up architecture of the network
 x = tf.placeholder(tf.float32, [None, 1024])
 
 # variable means the var will be changed during runtime
-nhid = 200 # number of hidden units
+nhid = 700 # number of hidden units
+alpha = 0.00001
+max_iter = 30000
+mini_batch_size = 20
+lam = 0.005 # right now lamda is set to 0. Need to change it for part 8?
+
+"""
+# At iteration 9500, Results of changing: 
+nhid: 
+(100, 0.877, 37), (200, 0.888, 6.47), (300, 0.9, 2.29), (400, 0.9, 1.12), (500, 0.9, 0.6)
+-> pick 300 hidden units b/c one of the best results and less unit = less chance of overfitting
+
+mini_batch:
+(20, 0.9, 12.7), (30, 0.88, 3.37) (40, 0.88, 2.22), (50, 0.9, 2.29), (60, 0.9, 1.54), (70, 0.9, 1.34)
+-> pick 50 b/c it has 0.9 performance and not too big/small size
+
+parameters used for part 7:
+nhid = 300 # number of hidden units
+alpha = 0.00001
+max_iter = 2000
+mini_batch_size = 50
+lam = 0.0000 
+plot from 0 to 2000, every 200
+
+"""
 
 """
 W0 = tf.Variable(tf.random_normal([1024, nhid], stddev=0.01))
@@ -109,11 +125,11 @@ W1 = tf.Variable(tf.random_normal([nhid, 6], stddev=0.01))
 b1 = tf.Variable(tf.random_normal([6], stddev=0.01))
 """
 
-W0 = tf.Variable(np.random.normal(0.0, 1.0, (1024, nhid)).astype(float32)/math.sqrt(1024 * nhid))
-b0 = tf.Variable(np.random.normal(0.0, 1.0, (nhid)).astype(float32)/math.sqrt(nhid))
+W0 = tf.Variable(np.random.normal(0.0, 0.1, (1024, nhid)).astype(float32)/math.sqrt(1024 * nhid))
+b0 = tf.Variable(np.random.normal(0.0, 0.1, (nhid)).astype(float32)/math.sqrt(nhid))
 
-W1 = tf.Variable(np.random.normal(0.0, 1.0, (nhid, 6)).astype(float32)/math.sqrt(6 * nhid))
-b1 = tf.Variable(np.random.normal(0.0, 1.0, (6)).astype(float32)/math.sqrt(6))
+W1 = tf.Variable(np.random.normal(0.0, 0.1, (nhid, 6)).astype(float32)/math.sqrt(6 * nhid))
+b1 = tf.Variable(np.random.normal(0.0, 0.1, (6)).astype(float32)/math.sqrt(6))
 
 layer1 = tf.nn.tanh(tf.matmul(x, W0)+b0)
 layer2 = tf.matmul(layer1, W1)+b1
@@ -125,38 +141,62 @@ y_ = tf.placeholder(tf.float32, [None, 6])
 # regularization/penalty
 # according to class, weight penalty is used when the network is overfitting
 # to create overfitting in this current architecture, can increase number of neurons (nhid)?
-lam = 0.0000 # right now lamda is set to 0. Need to change it for part 8?
+
 decay_penalty =lam*tf.reduce_sum(tf.square(W0))+lam*tf.reduce_sum(tf.square(W1))
 reg_NLL = -tf.reduce_sum(y_*tf.log(y))+decay_penalty
 
-train_step = tf.train.AdamOptimizer(0.0005).minimize(reg_NLL)
+train_step = tf.train.AdamOptimizer(alpha).minimize(reg_NLL)
 
 """
 Done setting up architecture. Actually run the network now
 """
 # init will init W0, B0, W1, B1 to random value
-init = tf.global_variables_initializer()
+#init = tf.global_variables_initializer()
+init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
 
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
 x_test, y_test = get_whole_set(act, "part7_test")
 x_val, y_val = get_whole_set(act, "part7_validation")
+x_train, y_train = get_whole_set(act, "part7_training")
 
+x_axis = np.arange(11) * 200
+train_performance = []
+test_performance = []
+val_performance = []
 
-for i in range(30000):
-  batch_xs, batch_ys = get_train_batch(act, 30) # <-change size of mini batch here. Max is 75 for now
+for i in range(max_iter+1):
+  batch_xs, batch_ys = get_train_batch(mini_batch_size, x_train, y_train) # <-change size of mini batch here. Max is 75 for now
   sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
   
   
-  if i % 500 == 0:
+  if i % 1000 == 0:
     print ("i=",i)
+    print ("Cost:", sess.run(reg_NLL, feed_dict={x: x_train, y_:y_train}))
+    acc_tr = sess.run(accuracy,feed_dict={x: x_train, y_: y_train})
+    train_performance.append(acc_tr * 100)
+    print ("Train:", acc_tr)
     
-    batch_xs, batch_ys = get_whole_set(act, "part7_training")
-    print ("Train:", sess.run(accuracy,feed_dict={x: batch_xs, y_: batch_ys}))
-    
-    print ("Test:", sess.run(accuracy, feed_dict={x: x_test, y_: y_test}))
-    print ("Validation:", sess.run(accuracy,feed_dict={x: x_val, y_: y_val}))
+    acc_t = sess.run(accuracy, feed_dict={x: x_test, y_: y_test})
+    test_performance.append(acc_t * 100)
+    print ("Test:", acc_t)
+
+    acc_v = sess.run(accuracy,feed_dict={x: x_val, y_: y_val})
+    val_performance.append(acc_v * 100)
+    print ("Validation:", acc_v)
 
     print ("Penalty:", sess.run(decay_penalty))
+
+"""
+plt.ylim(0,110)
+plt.plot(x_axis, test_performance, label="test")
+plt.plot(x_axis, train_performance, label="training")
+plt.plot(x_axis, val_performance, label="validation")
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
+plt.xlabel('Iteration')
+plt.ylabel('Correctness(%)')
+plt.savefig("part7.png")
+"""
